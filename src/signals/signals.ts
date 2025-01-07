edbfc9c038ba4ab33914d1f36ea52fc9fca1be15
ppavia <pierre.pavia@assemblee-nationale.fr>
2025-01-07 13:13:56 +0100

    Define base state data

---
+++ src/signals/signals.ts
@@ -0,0 +1,12 @@
+import { Signal, signal } from "@angular/core";
+import { VotingMethod } from "../datamodel/constants";
+import { Bulletin, Candidat } from "../datamodel/classes";
+
+type id = number;
+
+export const votingMethod: Signal<VotingMethod|null> = signal(null);
+export const attributionMethod: Signal<null> = signal(null);
+export const candidats: Signal<ReadonlyMap<id, Candidat>> = signal(new Map());
+export const bulletins: Signal<ReadonlyMap<id, Bulletin>> = signal(new Map());
+export const nbElecteursManuel: Signal<number|null> = signal(null);
+export const nNotes: Signal<number> = signal(5);
