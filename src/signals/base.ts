c594c8bca13e5e9596e758ef84b4a1165eb49634
ppavia <pierre.pavia@assemblee-nationale.fr>
2025-01-08 16:15:07 +0100

    Remove dummy type

--- src/signals/base.ts
+++ src/signals/base.ts
@@ -3,21 +3,19 @@ import { VotingMethod } from "../datamodel/constants";
 import { Bulletin, Candidat } from "../datamodel/classes";
 import { sortMap } from "../utils/utils";
 
-type id = number;
-
 // base versions
 const votingMethod: WritableSignal<VotingMethod|null> = signal(null);
 const attributionMethod: WritableSignal<null> = signal(null);
-const candidats: WritableSignal<Map<id, Candidat>> = signal(new Map(), {equal: Map.prototype.equals});
-const bulletins: WritableSignal<Map<id, Bulletin>> = signal(new Map(), {equal: Map.prototype.equals});
-const votes: WritableSignal<Map<id, number>> = signal(new Map(), {equal: Map.prototype.equals});
+const candidats: WritableSignal<Map<number, Candidat>> = signal(new Map(), {equal: Map.prototype.equals});
+const bulletins: WritableSignal<Map<number, Bulletin>> = signal(new Map(), {equal: Map.prototype.equals});
+const votes: WritableSignal<Map<number, number>> = signal(new Map(), {equal: Map.prototype.equals});
 const nbElecteursManuel: WritableSignal<number|null> = signal(null);
 const nNotes: WritableSignal<number> = signal(5);
 
 // readonly versions, exported under the base name
-const candidats_r = candidats.asReadonly() as Signal<ReadonlyMap<id, Candidat>>;
-const bulletins_r = bulletins.asReadonly() as Signal<ReadonlyMap<id, Bulletin>>;
-const votes_r = votes.asReadonly() as Signal<ReadonlyMap<id, number>>;
+const candidats_r = candidats.asReadonly() as Signal<ReadonlyMap<number, Candidat>>;
+const bulletins_r = bulletins.asReadonly() as Signal<ReadonlyMap<number, Bulletin>>;
+const votes_r = votes.asReadonly() as Signal<ReadonlyMap<number, number>>;
 export {
     votingMethod,
     attributionMethod,
@@ -40,24 +38,24 @@ export function deleteCandidat(cid: number) {
         candidats.set(candidatsValue);
     }
 }
-export function setBulletin(bid: id, bulletin: Bulletin) {
+export function setBulletin(bid: number, bulletin: Bulletin) {
     const bulletinsValue = bulletins();
     bulletinsValue.set(bid, bulletin);
     bulletins.set(bulletinsValue);
 }
-export function setVote(bid: id, nVotes: number) {
+export function setVote(bid: number, nVotes: number) {
     const votesValue = votes();
     votesValue.set(bid, nVotes);
     votes.set(votesValue);
 }
-export function sortBulletins(orderedIds: Iterable<id>, update = false) {
+export function sortBulletins(orderedIds: Iterable<number>, update = false) {
     sortMap(bulletins(), orderedIds);
     // TODO: trace what needs to be refreshed on bulletinsReordered and what doesn't
     if (update) {
         bulletins.set(bulletins());
     }
 }
-export function deleteBulletin(bid: id) {
+export function deleteBulletin(bid: number) {
     const bulletinsValue = bulletins();
     const votesValue = votes();
     if (bulletinsValue.delete(bid)) {
@@ -67,7 +65,7 @@ export function deleteBulletin(bid: id) {
         votes.set(votesValue);
     }
 }
-function crementVote(bid: id, crem: number) {
+function crementVote(bid: number, crem: number) {
     const votesValue = votes();
     if (!votesValue.has(bid)) {
         throw new Error(`id ${bid} not found among votes`);
@@ -75,9 +73,9 @@ function crementVote(bid: id, crem: number) {
     votesValue.set(bid, votesValue.get(bid)! + crem);
     votes.set(votesValue);
 }
-export function incrementVote(bid: id) {
+export function incrementVote(bid: number) {
     crementVote(bid, 1);
 }
-export function decrementVote(bid: id) {
+export function decrementVote(bid: number) {
     crementVote(bid, -1);
 }
