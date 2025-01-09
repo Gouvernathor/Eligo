5493cfddd6fe21083d938bc22b3c0610ca7ff254
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-01-09 23:36:40 +0100

    Deport house (stub) to its own component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ElectionComponent } from "../election/election.component";
+import { HouseComponent } from "../house/house.component";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
-  imports: [ElectionComponent]
+  imports: [ElectionComponent, HouseComponent]
 })
 export class MainComponent {
 }
