4e0ed015186ebe4cf58c01f7b68fbb25f6f633b0
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-01-10 16:45:39 +0100

    Use tabs

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ElectionComponent } from "../election/election.component";
 import { HouseComponent } from "../house/house.component";
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
-  imports: [ElectionComponent, HouseComponent]
+  imports: [NgbNavModule, ElectionComponent, HouseComponent]
 })
 export class MainComponent {
 }
