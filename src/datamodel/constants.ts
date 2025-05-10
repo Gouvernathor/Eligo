227b0fca755d5ed144a1a858ee322fc39349d660
Asriel Senna <aanonyme.personne@hotmail.fr>
2025-05-10 05:35:44 +0200

    Reorganize the data

--- src/datamodel/constants.ts
+++ src/datamodel/constants.ts
@@ -35,6 +35,40 @@ export namespace VotingMethod {
     export const values = () => [UNIQUE, APPROBATION, CLASSEMENT, NOTES];
 }
 
+type AttributionMethodBuilder = <Party>({ nSeats }: {
+    nSeats: number;
+}) => Attribution<Party, any> & HasNSeats;
+
+// the tips are stored in a separate map
+export const attributionMethods: Readonly<Record<BallotKind, Readonly<Record<string, AttributionMethodBuilder>>>> = [
+    {
+        MAJO: plurality,
+        // superMajority,
+
+        DHONDT: dHondt, // jefferson
+        WEBSTER: webster, // sainteLague
+        HHILL: (o) => huntingtonHill({ threshold: 0, ...o }),
+        HARE: hareLargestRemainders, // hamilton
+
+        // randomize
+    },
+    {
+        STV: instantRunoff,
+        BORDA: bordaCount,
+        CONDOR: condorcet,
+    },
+    {
+        MEAN: averageScore,
+        MEDIAN: medianScore,
+    },
+];
+// maybe move that to the component
+export const tips: Readonly<Record<string, string>> = {
+    STV: "Chaque électeur classe les candidats par ordre de préférence. Tant qu'aucun candidat n'est en tête sur une majorité de bulletins, candidat étant en tête du plus petit nombre de bulletins est élimité.",
+    CONDOR: "Chaque électeur classe les candidats par ordre de préférence. Des duels sont simulés entre chaque paire de candidats. Si un candidat gagne tous ses duels, il est élu.",
+};
+
+// deprecated
 export interface AttributionMethod {
     readonly ballotType: BallotKind;
     readonly attribution: <Party>({ nSeats }: {
